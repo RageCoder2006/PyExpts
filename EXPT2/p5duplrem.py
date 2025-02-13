4b68ae370d2e5a6087ef101c488f84feff25e32f
rishitbaitule <rishitbaitule@outlook.com>
2025-02-13 22:36:30 +0530

    p6 done and some edits

--- EXPT2/p5duplrem.py
+++ EXPT2/p5duplrem.py
@@ -1,4 +1,4 @@
-duplst = ["banana","apple","orange","banana","orange","grapefruit","orange"]
+duplst = ["banana", "apple", "orange", "banana", "orange", "grapefruit", "orange"]
 revlst = []
 for i in duplst:
     if i not in revlst:
---
+++ EXPT2/p6lstclone.py
@@ -0,0 +1,7 @@
+lst = ['banana', 'apple', 'orange', 'grapefruit']
+clone = []
+
+for i in lst:
+    clone.append(i)
+
+print(f"Cloned List: {clone}")
