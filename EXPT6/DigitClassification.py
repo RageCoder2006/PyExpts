6e245dc36b39ee5c417c364951204ba28999910b
RageCoder2006 <rishitbaitule@outlook.com>
2025-03-26 22:16:20 +0530

    fixed an error

--- EXPT6/DigitClassification.py
+++ EXPT6/DigitClassification.py
@@ -2,12 +2,13 @@
 
 num = int(input("Enter the number between 1 to 9,999: "))
 count = 0
+numog = num
 
 while num > 0:
     num = num // 10
     count += 1
 
-if count == 1:
+if count == 1 or numog == 0:
     print("Single Digit")
 elif count == 2:
     print("Two Digits")
